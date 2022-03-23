6a44df9fd768e83bcefe92145c078f39902ff7b6
LoTwT <709937065@qq.com>
2022-03-23 17:39:55 +0800

    feat: SearchBar

--- components/p_home/SearchBar.tsx
+++ components/p_home/SearchBar.tsx
@@ -1,5 +1,16 @@
-const SearchBar = () => {
-  return <div>SearchBar</div>
-}
+import Link from 'next/link'
+import s from './SearchBar.module.css'
 
+const SearchBar = () => {
+  return (
+    <div className={s.wrapper}>
+      <Link href="/search">
+        <a className={s.search}>输入搜索内容</a>
+      </Link>
+      <Link href="/user">
+        <a className={s.user} />
+      </Link>
+    </div>
+  )
+}
 export default SearchBar
