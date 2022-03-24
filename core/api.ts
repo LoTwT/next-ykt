4bdbca9be50f410094813af12cea1043c6dd8eee
LoTwT <709937065@qq.com>
2022-03-24 17:20:55 +0800

    feat: recommendApi

--- core/api.ts
+++ core/api.ts
@@ -1,4 +1,5 @@
 import { IHomeResponse } from 'pages/api/home'
+import { IRecommendResponse } from 'pages/api/recommend'
 import request from './request'
 
 // 首页
@@ -6,4 +7,12 @@ export const getHome = () =>
   request.request<IHomeResponse>({
     url: '/home',
     method: 'get',
+  })
+
+// 首页推荐
+export const getRecommend = (params: { start: number; offset: number }) =>
+  request.request<IRecommendResponse>({
+    url: '/recommend',
+    method: 'get',
+    params,
   })
