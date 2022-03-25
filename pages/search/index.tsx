83d3ddf8a0a49774d07d87ae4e938eb18a751f38
LoTwT <709937065@qq.com>
2022-03-25 17:58:00 +0800

    feat(Search): base & SearchContent

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,7 +1,35 @@
 import { NextPage } from 'next'
+import { useState } from 'react'
+
+const TYPES = {
+  HISTORY: 'history',
+  SUGGEST: 'suggest',
+  RESULT: 'result',
+} as const
 
 const Search: NextPage = () => {
-  return <div>Search</div>
+  // 内容类型
+  const [contType, setContType] =
+    useState<typeof TYPES[keyof typeof TYPES]>('history')
+
+  const map = {
+    [TYPES.HISTORY]: <div>history</div>,
+    [TYPES.SUGGEST]: <div>suggest</div>,
+    [TYPES.RESULT]: <div>result</div>,
+  }
+
+  const renderContent = () => {
+    return map[contType]
+  }
+
+  return (
+    <div>
+      {/* 搜索框 */}
+
+      {/* 内容 */}
+      {renderContent()}
+    </div>
+  )
 }
 
 export default Search
