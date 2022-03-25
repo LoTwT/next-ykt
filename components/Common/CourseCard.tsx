68af8307593db7d19f758507b88383202439b09b
LoTwT <709937065@qq.com>
2022-03-25 13:57:44 +0800

    feat(CourseCard): time

--- components/Common/CourseCard.tsx
+++ components/Common/CourseCard.tsx
@@ -8,16 +8,28 @@ interface IProps {
 }
 
 const CourseCard = ({ data }: IProps) => {
-  const { courseTitle, id, categoryName } = data
+  const {
+    courseTitle,
+    id,
+    categoryName,
+    courseTime = '随到随学',
+    lessonNum,
+  } = data
 
   return (
-    <Link href="/course/detail/id[]" as={`/course/detail/${id}`} passHref>
+    <Link href="/course/detail/[id]" as={`/course/detail/${id}`} passHref>
       <a className={`${styles.card} border-b-1px`}>
         {/* 标题 */}
         <h5>
           <span className={styles.categoryTag}>{categoryName}</span>
           <span className={styles.title}>{courseTitle}</span>
         </h5>
+
+        {/* 开课时间 */}
+        <div className={styles.time}>
+          <span>开课时间：&nbsp;&nbsp;{courseTime}</span>
+          <span className={styles.lessonNum}>{lessonNum}课时</span>
+        </div>
       </a>
     </Link>
   )
