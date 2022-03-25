b253ca2e0ab76621b1d95456844b51045144cdab
LoTwT <709937065@qq.com>
2022-03-25 13:51:12 +0800

    feat(CourseCard): header

--- components/Common/CourseCard.tsx
+++ components/Common/CourseCard.tsx
@@ -8,11 +8,17 @@ interface IProps {
 }
 
 const CourseCard = ({ data }: IProps) => {
-  const { courseTitle, id } = data
+  const { courseTitle, id, categoryName } = data
 
   return (
     <Link href="/course/detail/id[]" as={`/course/detail/${id}`} passHref>
-      <a className={`${styles.card} border-b-1px`}>{courseTitle}</a>
+      <a className={`${styles.card} border-b-1px`}>
+        {/* 标题 */}
+        <h5>
+          <span className={styles.categoryTag}>{categoryName}</span>
+          <span className={styles.title}>{courseTitle}</span>
+        </h5>
+      </a>
     </Link>
   )
 }
